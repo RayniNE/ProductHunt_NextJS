eef93b9c98c760923cd8cdadaae6d4f027067bf7
Rayni Nuñez <timetopwm@gmail.com>
2020-08-28 20:43:12 -0400

    Se han creado los componentes login, crear-cuenta, nuevo-producto y populares. Se comenzo a dar estilos a los distintos componentes con styled y css. Se creo el archivo Boton, Buscar y Formulario en la carpeta UI, para poner los estilos que tendran los elementos de con el mismo nombre.

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,34 +1,96 @@
 import React from 'react';
 import Buscar from '../UI/Buscar';
 import Navegacion from './Navegacion';
+import Boton from '../UI/Boton';
 import Link from 'next/link';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+
+const ContenedorHeader = styled.div`
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+    @media (min-width: 768px){
+        display: flex;
+        justify-content: space-between;
+    }
+`;
+
+const Logo = styled.p`
+    color: var(--naranja);
+    font-size: 4rem;
+    line-height: 0;
+    font-weight: 700;
+    font-family: 'Roboto Slab', serif;
+    margin-right: 2rem;
+`;
 
 const Header = () => {
 
-    return (
-        <header>
-            <div>
-                <div>
-                    <p> P </p>
+    const usuario = false;
 
-                    {/* Buscador aqui. */}
+    return (
+        <header
+            css={css`
+                border-bottom: 2px solid var(--gris3);
+                padding: 1rem 0;
+
+            `}
+        >
+            <ContenedorHeader>
+                <div
+                    css={css`
+                        display: flex;
+                        align-items: center;
+                    `}
+                >
+
+                    <Link href="/">
+                        <Logo> P </Logo>
+                    </Link>
+
                     <Buscar />
 
-                    {/* Nav aqui. */}
                     <Navegacion />
                 </div>
 
-                <div>
+                <div
+                    css={css`
+                        display: flex;
+                        align-items: center;
+                    `}
+                >
 
-                    <p> Hola: Rayni </p>
 
-                    <button type="button"> Cerrar sesión </button>
+                    {usuario ? (
+                        <>
+                            <p
+                                css={css`
+                                     margin-right: 2rem;
+                                    `}
+                            > Hola: Rayni </p>
 
-                    <Link href="/"> Iniciar sesión </Link>
-                    <Link href="/"> Crear cuenta </Link>
+                            <Boton type="button" bgColor="true"> Cerrar sesión </Boton>
+                        </>
+                    ) : (
+                            <>
+                                <Link href="/login">
+                                    <Boton bgColor="true"> Iniciar sesión </Boton>
+                                </Link>
+
+                                <Link href="/crear-cuenta">
+                                    <Boton> Crear cuenta </Boton>
+                                </Link>
+                            </>
+                        )}
+
+
+
+
+
 
                 </div>
-            </div>
+            </ContenedorHeader>
         </header>
     );
 }
---
+++ pages/nuevo-producto.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from '../components/layout/Layout';
+
+const NuevoProducto = () => {
+    return (
+        <div>
+
+            <Layout>
+
+                <h1> Crear Cuenta </h1>
+
+            </Layout>
+
+        </div >
+    )
+}
+export default NuevoProducto; NuevoProducto
